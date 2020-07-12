b1531ad8677d5f097d287e8f8c169f9cd08f47f6
shrey27tri01 <shreytripathi27.com>
2020-07-12 17:43:22 +0530

    one change

--- blogonthego/urls.py
+++ blogonthego/urls.py
@@ -31,7 +31,7 @@ urlpatterns = [
     path('articles/', include('articles.urls')),
     path('about/', views.about),
     path('', article_views.article_list, name='home'),
-    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('images/favicon.ico')))
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('favicon.ico')))
 ]
 
 urlpatterns += staticfiles_urlpatterns()
